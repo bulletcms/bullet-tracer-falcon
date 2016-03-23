9131b80f1d3ec140cf05a0176192d0ee91071891
Kevin Wang <wangkevin448@gmail.com>
2016-03-22 19:03:32 -0700

    bulletmark

--- src/js/controllers/bulletmark.js
+++ src/js/controllers/bulletmark.js
@@ -1,8 +1,17 @@
-const Bulletmark = (viewtree)=>{
-  // view tree : json component array
-  return <div>
-    I am a view tree.
-  </div>;
+const Bulletmark = (viewjson, views)=>{
+  // view json : json component array; views : map of tag name to component jsx [ex: 'thecomponent': TheComponent]
+  let viewtree = [];
+
+  for(let i of viewjson){
+    let k;
+    if(views[i.component]){
+      viewtree.push(views[i.component]);
+    } else {
+      viewtree.push('');
+    }
+  }
+
+  return viewtree;
 };
 
 export {Bulletmark};
